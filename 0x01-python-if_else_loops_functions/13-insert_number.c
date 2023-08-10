a4727265fac48c8070f50987e94025d677dbee49
Kwadwo7 <appiahbenedict724@gmail.com>
2023-08-10 05:59:53 +0000

    update3

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,7 +7,8 @@
  *     *
  *      * Return: If the function fails - NULL.
  *       * Otherwise - a pointer to the new node.
- *        */
+ */
+
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head, *new;
@@ -27,8 +28,8 @@ listint_t *insert_node(listint_t **head, int number)
 	while (node && node->next && node->next->n < number)
 		node = node->next;
 
-		new->next = node->next;
-		node->next = new;
+	new->next = node->next;
+	node->next = new;
 
 	return (new);
 }
